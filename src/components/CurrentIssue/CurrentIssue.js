9f0c933771bd2b286704fe32228b5f5bba28f78d
Ritesh Kumar <riteshkumarstudy@gmail.com>
2021-05-06 00:28:24 +0530

    CurrentIssue Frontend Updated

--- src/components/CurrentIssue/CurrentIssue.js
+++ src/components/CurrentIssue/CurrentIssue.js
@@ -9,6 +9,19 @@ function CurrentIssue() {
 
   return (
     <div className="current-issues">
+      <div>
+        <p>Assigned<span>Username</span></p>
+        <p>Priority<span>High</span></p>
+        <p>Description</p>
+        <p className="description-text">
+        src\App.js
+          Line 1:26:  'useEffect' is defined but never used             
+          Line 9:20:  'setAddIssue' is assigned a value but never used  
+        src\components\AddIssue\AddIssue.js
+          Line 1:26:  'useEffect' is defined but never used             
+          Line 8:20:  'setAddIssue' is assigned a value but never used   
+        </p>
+      </div>
 
    </div>
   );
